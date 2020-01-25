d0dec00a55743956da76ab2acc6e019da2ec0628
szymonklo <58526555+szymonklo@users.noreply.github.com>
2020-01-25 12:41:26 +0100

    version 2 - operation on temporaty List, AddRange, Skip, Take

--- activityNotifications/Program.cs
+++ activityNotifications/Program.cs
@@ -22,28 +22,75 @@ class Solution
         int i = d;  //3
         int notifications = 0;
         List<int> lastExp = expenditure.Take(d).OrderBy(x => x).ToList();   //[10, 20,30]
+        List<int> newLastExp = new List<int>();
+
         if (d % 2 != 0)
         {
             int index = (d - 1) / 2; //1
             for (; i < n; i++)
             {
-                lastExp = expenditure.Take(d).OrderBy(x => x).ToList();   //[10, 20,30]
-                int median = lastExp[index];    //20
-                if (expenditure[i] > median)    //40
+                int exp = expenditure[i];
+                int median2 = 2 * lastExp[index];    //20
+                if (exp >= median2)    //40
+                {
+                    Console.WriteLine(exp + " " + median2);
                     notifications++;
+                }
+                for (int j = 1; j < lastExp.Count; j++)
+                {
+                    if (exp < lastExp[j])
+                    {
+                        newLastExp.Clear();
+                        newLastExp.AddRange(lastExp.Skip(1).Take(j - 1));
+                        newLastExp.Add(exp);
+                        newLastExp.AddRange(lastExp.Skip(j).Take(j - 1));
+                        lastExp.Clear();
+                        lastExp.AddRange(newLastExp);
+                        break;
+                    }
+                }
+                if (exp > lastExp[(lastExp.Count - 1)])
+                {
+                    //newLastExp.Clear();
+                    List<int> newLastExp2 = new List<int>(lastExp.Skip(1).Take(lastExp.Count - 1));
+                    newLastExp2.Add(exp);
+                    lastExp.Clear();
+                    lastExp.AddRange(newLastExp2);
+                }
             }
         }
         else
         {
-            int index1 = d/2-1; //1
-            int index2 = d/2; //1
+            int index1 = d/2-1;
+            int index2 = d/2;
 
             for (; i < n; i++)
             {
-                lastExp = expenditure.Take(d).OrderBy(x => x).ToList();   //[10, 20,30]
-                int median = (lastExp[index1]+ lastExp[index2])/2;    //20
-                if (expenditure[i] > median)    //40
+                int exp = expenditure[i];
+                int median2 = 2 * (lastExp[index1]+ lastExp[index2])/2;    //20
+                if (expenditure[i] >= median2)
                     notifications++;
+                for (int j = 1; j < lastExp.Count; j++)
+                {
+                    if (exp < lastExp[j])
+                    {
+                        newLastExp.Clear();
+                        newLastExp.AddRange(lastExp.Skip(1).Take(j - 1));
+                        newLastExp.Add(exp);
+                        newLastExp.AddRange(lastExp.Skip(j).Take(j - 1));
+                        lastExp.Clear();
+                        lastExp.AddRange(newLastExp);
+                        break;
+                    }
+                }
+                if (exp > lastExp[(lastExp.Count - 1)])
+                {
+                    //newLastExp.Clear();
+                    List<int> newLastExp2 = new List<int>(lastExp.Skip(1).Take(lastExp.Count - 1));
+                    newLastExp2.Add(exp);
+                    lastExp.Clear();
+                    lastExp.AddRange(newLastExp2);
+                }
             }
         }
         return notifications;
